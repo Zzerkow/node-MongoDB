0a6bcc8a2d0576d7e7771b5963be4cdb6b4c01c9
Neoznzoe <bessonvictor59@gmail.com>
2025-04-04 14:11:59 +0200

    feat(delete): add delete button'

--- app.js
+++ app.js
@@ -13,22 +13,18 @@ const PORT = process.env.PORT || 3001;
 
 const sslOptions = {
   key: fs.readFileSync(path.join(__dirname, 'keys', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'keys', 'cert.pem')) // Remplacez par le chemin de votre certificat
+  cert: fs.readFileSync(path.join(__dirname, 'keys', 'cert.pem'))
 };
 
-// Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'vue')));
 
-// Connexion MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connecté !'))
   .catch(err => console.error('Erreur MongoDB :', err));
 
-// Routes API
 app.use('/api/locations', locationRoutes);
 
-// Lancer le serveur
 https.createServer(sslOptions, app).listen(PORT, () => {
   console.log(`Serveur sécurisé en HTTPS sur le port ${PORT}`);
 });
