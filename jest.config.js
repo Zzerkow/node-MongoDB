b8af22b117f6e93a9bec75c5b3b22d32c5742e6f
Filsnoel <leo.filsnoel@gmail.com>
2025-04-04 17:44:56 +0200

    add tests

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,35 @@
 module.exports = {
   // The test environment that will be used for testing
   testEnvironment: 'node',
-  
+
   // The glob patterns Jest uses to detect test files
   testMatch: ['**/__tests__/**/*.test.js'],
-  
+
   // Setup file that runs before all tests
-  // Use setup-mock.js instead of setup.js to avoid MongoDB Memory Server issues
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setup-mock.js'],
-  
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup-mongoose.js'],
+
   // Indicates whether each individual test should be reported during the run
   verbose: true,
-  
+
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
-  
+
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-  
+
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-  
+
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ['/node_modules/'],
-  
+
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text', 'lcov'],
-  
+
   // The maximum amount of workers used to run your tests
   maxWorkers: '50%',
-  
+
   // A timeout value in milliseconds for tests
   testTimeout: 10000
 };
