605b01fe836f07b9c31ade03aa603ed3fb4a642e
Filsnoel <leo.filsnoel@gmail.com>
2025-04-04 15:09:42 +0200

    add times series structure and create a locationController file

---
+++ controllers/locationController.js
@@ -0,0 +1,48 @@
+const Location = require('../models/LocationModel');
+
+exports.createLocation = async (req, res) => {
+  try {
+    const location = new Location(req.body);
+    await location.save();
+    res.status(201).json(location);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getAllLocations = async (req, res) => {
+  try {
+    const locations = await Location.find();
+    res.json(locations);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getLocationById = async (req, res) => {
+  try {
+    const location = await Location.findById(req.params.id);
+    if (!location) return res.status(404).json({ error: 'Not found' });
+    res.json(location);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateLocation = async (req, res) => {
+  try {
+    const location = await Location.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(location);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.deleteLocation = async (req, res) => {
+  try {
+    await Location.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Location deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ controllers/timeSeriesController.js
@@ -0,0 +1,1 @@
+const TimesSeriesModel = require('../models/TimesSeriesModel');
